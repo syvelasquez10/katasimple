ca91099aa98ffe9b944f0758865b83dc75bdf0bc
Sergio Velasquez <sy.velasquez10@uniandes.edu.co>
2018-09-30 14:17:24 -0500

    Verde: Iteracion 1, se soluciona test de longitud cadena de un elemento

--- SecuenciaNumeros.py
+++ SecuenciaNumeros.py
@@ -3,4 +3,6 @@ class SecuenciaNumeros:
     def retornarlongitud(self, cadena):
         if cadena == '':
             return 0
+        elif ',' not in cadena:
+            return 1
         return -1
--- test_secuenciaNumeros.py
+++ test_secuenciaNumeros.py
@@ -4,5 +4,5 @@ from SecuenciaNumeros import SecuenciaNumeros
 class TestSecuenciaNumeros(TestCase):
     def test_retornarlongitud(self):
         self.assertEqual(SecuenciaNumeros().retornarlongitud(''), 0, 'cadena vacia')
-        self.assertEqual(SecuenciaNumeros().retornarlongitud('1'), 1, 'cadena de un elemento')
+        self.assertEqual(SecuenciaNumeros().retornarlongitud('107'), 1, 'cadena de un elemento')
 
