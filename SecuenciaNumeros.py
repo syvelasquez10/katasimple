399d83a72603dd1ac3867e421c82d4f3d59ec0c1
Sergio Velasquez <sy.velasquez10@uniandes.edu.co>
2018-09-30 14:29:38 -0500

    Verde: Iteracion 1, se soluciona test de longitud cadena de n elementos

--- SecuenciaNumeros.py
+++ SecuenciaNumeros.py
@@ -3,8 +3,6 @@ class SecuenciaNumeros:
     def retornarlongitud(self, cadena):
         if cadena == '':
             return 0
-        elif ',' not in cadena:
-            return 1
-        elif cadena.count(',') == 1:
-            return 2
-        return -1
+        else:
+            cadena_numeros = cadena.split(',')
+            return len(cadena_numeros)
