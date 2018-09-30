8b536a28a6544cbedce59bc3b12a77e806f8cc90
Sergio Velasquez <sy.velasquez10@uniandes.edu.co>
2018-09-30 15:06:12 -0500

    Rojo: Iteracion 2, falla test de minimo en cadena de n elementos

--- test_secuenciaNumeros.py
+++ test_secuenciaNumeros.py
@@ -8,7 +8,7 @@ class TestSecuenciaNumeros(TestCase):
         self.assertEqual(SecuenciaNumeros().retornarlongitud(''), 0, 'cadena vacia')
         self.assertEqual(SecuenciaNumeros().retornarlongitud('12.5'), 1, 'cadena de un elemento')
         self.assertEqual(SecuenciaNumeros().retornarlongitud('12,5'), 2, 'cadena de dos elementos')
-        self.assertEqual(SecuenciaNumeros().retornarlongitud('12,5,4,2'), 4, 'cadena de n elementos igual ')
+        self.assertEqual(SecuenciaNumeros().retornarlongitud('12,5,4,2'), 4, 'cadena de n elementos igual')
         self.assertNotEqual(SecuenciaNumeros().retornarlongitud('12,5,4,2'), 5, 'cadena de n elementos no igual')
 
 
@@ -16,6 +16,8 @@ class TestSecuenciaNumeros(TestCase):
         self.assertEqual(SecuenciaNumeros().minimo(''), 'vacio', 'cadena vacia minimo')
         self.assertEqual(SecuenciaNumeros().minimo('12'), 12, 'cadena de un elemento minimo')
         self.assertEqual(SecuenciaNumeros().minimo('12,8'), 8, 'cadena de dos elementos minimo')
+        self.assertEqual(SecuenciaNumeros().retornarlongitud('12,5,4,2'), 2, 'cadena de n elementos igual minimo')
+        self.assertNotEqual(SecuenciaNumeros().retornarlongitud('12,5,4,2'), 5, 'cadena de n elementos no igual minmo')
 
     def test_secuencia(self):
         self.assertEqual(SecuenciaNumeros().secuencia(''), str(SecuenciaNumeros().retornarlongitud(''))+','+str(SecuenciaNumeros().minimo('')), 'cadena vacia resultados')
