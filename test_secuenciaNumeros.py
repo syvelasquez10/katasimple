6cfb40bbb08b2d7d19aae0ee1d8ea4aa69fd8b39
Nelson Hurtado <njhurtado@gmail.com>
2018-09-30 14:21:11 -0500

    Rojo: Iteracion 1, falla test de longitud cadena de dos elementos

--- test_secuenciaNumeros.py
+++ test_secuenciaNumeros.py
@@ -4,5 +4,6 @@ from SecuenciaNumeros import SecuenciaNumeros
 class TestSecuenciaNumeros(TestCase):
     def test_retornarlongitud(self):
         self.assertEqual(SecuenciaNumeros().retornarlongitud(''), 0, 'cadena vacia')
-        self.assertEqual(SecuenciaNumeros().retornarlongitud('107'), 1, 'cadena de un elemento')
+        self.assertEqual(SecuenciaNumeros().retornarlongitud('12.5'), 1, 'cadena de un elemento')
+        self.assertEqual(SecuenciaNumeros().retornarlongitud('12,5'), 1, 'cadena de dos elementos')
 
