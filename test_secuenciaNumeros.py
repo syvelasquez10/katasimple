3f8d8100c39793791025edd80e39fe39af7acbb6
Nelson Hurtado <njhurtado@gmail.com>
2018-09-30 14:12:22 -0500

    Rojo: Iteracion 1, falla test de longitud cadena de un elemento

--- test_secuenciaNumeros.py
+++ test_secuenciaNumeros.py
@@ -4,4 +4,5 @@ from SecuenciaNumeros import SecuenciaNumeros
 class TestSecuenciaNumeros(TestCase):
     def test_retornarlongitud(self):
         self.assertEqual(SecuenciaNumeros().retornarlongitud(''), 0, 'cadena vacia')
+        self.assertEqual(SecuenciaNumeros().retornarlongitud('1'), 1, 'cadena de un elemento')
 
