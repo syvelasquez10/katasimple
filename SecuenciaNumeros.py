25ff40e9b9db814953e2a2488da4292316480522
Sergio Velasquez <sy.velasquez10@uniandes.edu.co>
2018-09-30 14:42:33 -0500

    Rojo: Iteracion 2, test de minimo en cadena de 0 elementos y test de arreglo de respuestas con el número de elementos y el minimo

--- SecuenciaNumeros.py
+++ SecuenciaNumeros.py
@@ -1,8 +1,14 @@
 class SecuenciaNumeros:
 
+    def secuencia(self,cadena):
+        return -1
+
     def retornarlongitud(self, cadena):
         if cadena == '':
             return 0
         else:
             cadena_numeros = cadena.split(',')
             return len(cadena_numeros)
+
+    def minimo(self, cadena):
+        return -1
--- test_secuenciaNumeros.py
+++ test_secuenciaNumeros.py
@@ -1,6 +1,8 @@
 from unittest import TestCase
 
 from SecuenciaNumeros import SecuenciaNumeros
+
+
 class TestSecuenciaNumeros(TestCase):
     def test_retornarlongitud(self):
         self.assertEqual(SecuenciaNumeros().retornarlongitud(''), 0, 'cadena vacia')
@@ -8,3 +10,13 @@ class TestSecuenciaNumeros(TestCase):
         self.assertEqual(SecuenciaNumeros().retornarlongitud('12,5'), 2, 'cadena de dos elementos')
         self.assertEqual(SecuenciaNumeros().retornarlongitud('12,5,4,2'), 4, 'cadena de n elementos igual ')
         self.assertNotEqual(SecuenciaNumeros().retornarlongitud('12,5,4,2'), 5, 'cadena de n elementos no igual')
+
+
+class TestSecuenciaNumeros(TestCase):
+    def test_minimo(self):
+        self.assertEqual(SecuenciaNumeros().minimo(''), 'vacio', 'cadena vacia')
+
+
+class TestSecuenciaNumeros(TestCase):
+    def test_secuencia(self):
+        self.assertEqual(SecuenciaNumeros().secuencia(''), str(SecuenciaNumeros().retornarlongitud(''))+','+str(SecuenciaNumeros().minimo('')), 'cadena vacia')
